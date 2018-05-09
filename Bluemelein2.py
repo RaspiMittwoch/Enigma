70b63ef31aa2727a522dfeb6af266149aa23e46f
DuceHD <36924097+RaspiMittwoch@users.noreply.github.com>
2018-05-09 14:31:41 +0200

    Add files via upload

--- Bluemelein2.py
+++ Bluemelein2.py
@@ -57,17 +57,17 @@ class Rollen:
         self.normal_letters.append(self.normal_letters[0])
         self.normal_letters.pop(0)
 
-    def clickback(self):
-        self.normal_letters.insert(0, self.normal_letters[-1])
-        self.normal_letters.pop(-1)
-
     def update_dic(self):
         for letter in self.abc:
             self.enc_dict.update({letter : self.normal_letters[self.num]})
             self.dec_dict.update({self.normal_letters[self.num]: letter})
             self.num = self.num + 1
         self.num = 0
-
+        
+    def click(self):
+        self.clickahead()
+        self.update_dic()
+        
 
     def __init__(self, stellung):
 
@@ -85,6 +85,63 @@ class Rollen:
         l2 = self.dec_dict[l]
         return l, l2
 
-def 
-e = Rollen(wsetting[0])
-print(e.encodedecodetest("a"))
+
+class Controller:
+    message = []
+    letters = []
+    
+    def split(self, phrase):
+        
+        
+        for let in phrase:
+            
+            self.letters.append(let)
+        return self.letters
+        
+    def __init__(self):
+        pass
+
+
+
+c = Controller()
+
+
+
+def control_encode(phrase):
+    r0 = Rollen(wsetting[0])
+    out_dic = []
+    out_string = ""
+    x = c.split(phrase)
+    for i in x:
+        out_dic.append(r0.encode(i))
+        r0.click()
+    for i in out_dic:
+        out_string = out_string + i
+    #print(out_string)
+    return out_string
+    
+def control_decode(phrase):
+    r0 = Rollen(wsetting[0])
+    out_dic = []
+    out_string = ""
+    x = c.split(phrase)
+    for i in x:
+        out_dic.append(r0.decode(i))
+        r0.click()
+    for i in out_dic:
+        out_string = out_string + i
+    #print(out_string)
+    return out_string
+
+
+out_encode = control_encode("aaaaaaa")
+out_decode = control_decode(out_encode)
+print(out_encode, out_decode)
+
+
+
+
+#e = Rollen(wsetting[0])
+#print(e.encodedecodetest("i"))
+#c = Controller()
+#print(c.split("abcasdfasöldjfalöksdfjöalksdkgjajskhfaölsgjakldghöaq"))
